eb00304dfe36337815bfc6826c761a345ff32386
tiro308 <tiro308@gmail.com>
2015-04-08 11:50:25 +0200

    tut bis ch.6
    ball mit pickups

---
+++ test1/Assets/scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+
+
+	/*
+	public float speed;
+	public float thrust;
+	public Rigidbody rb;
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate() {
+
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		rb.AddForce(moveHorizontal*speed, 0.0f, moveVertical*speed);
+	} */
+
+	public float speed;
+	public Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate(){
+
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		rb.AddForce (moveHorizontal*speed*Time.deltaTime,0.0f,moveVertical*speed*Time.fixedDeltaTime); 
+	}
+
+}
